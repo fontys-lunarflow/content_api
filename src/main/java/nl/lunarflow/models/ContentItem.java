0fe9a986ea100f63d010a92081e967124e0fe3c5
isakal <sakalivan4@gmail.com>
2025-04-15 12:03:35 +0200

    Tidying up
    - docker images
    - pom.xml
    - content item model
    - docker-compose.yml

--- src/main/java/nl/lunarflow/models/ContentItem.java
+++ src/main/java/nl/lunarflow/models/ContentItem.java
@@ -9,9 +9,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
-
-import jakarta.ws.rs.NotFoundException;
-
 import org.hibernate.validator.constraints.URL;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
